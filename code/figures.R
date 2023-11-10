4042c6b37f097e5d605deef60882e3c2cf4bd2ac
Nishka Sharma <71686549+nishkasharma@users.noreply.github.com>
2023-11-10 11:03:36 -0600

    Final tweaks to figures      - Chnaged colour of the India and China stack in the health        burden vs resources figure      - Changed the colour scheme of opportunity bands such that        the medium-high band is more blue now. These changes appear        in all maps      - Changed the registry map to show countries in their        opportunity band colour instead of a common yes/no colour.        Venezuela is coloured in light gray to indicate that it was        not included in the opportunity calculations.

--- code/figures.R
+++ code/figures.R
@@ -24,36 +24,41 @@ df1 <- data.frame(group = c("Europe, US \nand Canada",
                             "Asia, Africa and \nLatin America", 
                             "Asia, Africa and \nLatin America", 
                             "Asia, Africa and \nLatin America", 
+                            "Asia, Africa and \nLatin America", 
                             "Asia, Africa and \nLatin America"),
                   region = c("Europe, US \nand Canada", 
-                             "Asia (India \nand China)",
+                             "India and China",
                              "Asia (excluding \nIndia and China)",
                              "Africa",
-                             "Latin America"),
-                  measure = c("Regional share of all life \nyears lost due to PM2.5 globally", 
-                              "Regional share of all life \nyears lost due to PM2.5 globally", 
-                              "Regional share of all life \nyears lost due to PM2.5 globally", 
-                              "Regional share of all life \nyears lost due to PM2.5 globally", 
-                              "Regional share of all life \nyears lost due to PM2.5 globally"),
-                  value = c(4.1, 59.7, 22.6, 10.1, 3.4))
+                             "Latin America",
+                             "Asia"),
+                  measure = c("Regional share of all life \nyears lost due to PM2.5 globally*", 
+                              "Regional share of all life \nyears lost due to PM2.5 globally*", 
+                              "Regional share of all life \nyears lost due to PM2.5 globally*", 
+                              "Regional share of all life \nyears lost due to PM2.5 globally*", 
+                              "Regional share of all life \nyears lost due to PM2.5 globally*", 
+                              "Regional share of all life \nyears lost due to PM2.5 globally*"),
+                  value = c(4.1, 59.7, 22.6, 10.1, 3.4, 0))
 
 df1$region <- factor(df1$region, 
                           levels=c("Europe, US \nand Canada", 
                                    "Africa",
                                    "Latin America",
+                                   "Asia",
                                    "Asia (excluding \nIndia and China)",
-                                   "Asia (India \nand China)"))
+                                   "India and China"))
 
 p1.1 <- df1 %>% ggplot(aes(x = group, y = value , fill = region)) +
   facet_wrap(~measure, strip.position = "bottom") +
-  geom_bar(stat = 'identity', position = 'stack', width = 0.5) + 
+  geom_bar(stat = 'identity', position = 'stack', width = 0.5, colour = "black") + 
   labs(x = "", y = "", fill = "Region", title = "Health burden due to air pollution",
-       caption = "Data: Air Quality Life Index, OpenAQ, Clean Air Fund")  +
-  scale_fill_manual(values = c("Africa" = "#01497c", 
-                               "Latin America" = "#087e8b", 
+       caption = "Source: * Air Quality Life Index Annual Update, 2023")  +
+  scale_fill_manual(values = c("Africa" = "#01497C", 
+                               "Latin America" = "#6F98A2", 
                                "Europe, US \nand Canada" = "#935c5c", 
-                               "Asia (excluding \nIndia and China)" = "#9bc1bc", 
-                               "Asia (India \nand China)" = "#dde7c7")) +       # alt color #55828b
+                               "Asia (excluding \nIndia and China)" = "#DDE7C7", 
+                               "India and China" = "#e2e2e2",
+                               "Asia" = "#A6C0B4")) +       # alt color #55828b
   scale_y_continuous(limits = c(0,100)) +
   ggthemes::theme_tufte() +
   theme(legend.position = "left",
@@ -65,7 +70,7 @@ p1.1 <- df1 %>% ggplot(aes(x = group, y = value , fill = region)) +
         legend.box.margin = margin(b = 1, unit = "cm"),
         plot.subtitle = element_text(hjust = 0.5, size = 7),
         plot.caption = element_text(hjust = 0, size = 9, face = "italic"),
-        legend.key = element_rect(color = "black"),
+        # legend.key = element_rect(color = "black"),
         plot.background = element_rect(color = "white", fill = "white"),
         strip.text.x =  element_text(size = 14),
         strip.placement = "outside",   # format to look like title
@@ -79,47 +84,46 @@ df2 <- data.frame(group = c("Europe, US \nand Canada",
                             "Europe, US \nand Canada", 
                             "Asia, Africa and \nLatin America",
                             "Asia, Africa and \nLatin America", 
-                            "Asia, Africa and \nLatin America", 
                             "Asia, Africa and \nLatin America"),
                   region = c("Europe, US \nand Canada", 
-                             "Asia (India \nand China)",
+                             "India and China",
                              "Asia (excluding \nIndia and China)",
                              "Africa",
                              "Latin America",
                              "Europe, US \nand Canada", 
-                             "Asia (India \nand China)",
-                             "Asia (excluding \nIndia and China)",
+                             "Asia",
                              "Africa",
                              "Latin America"),
-                  measure = c("Regional share of all philanthropic \nfunding for AQ in 2021", 
-                              "Regional share of all philanthropic \nfunding for AQ in 2021", 
-                              "Regional share of all philanthropic \nfunding for AQ in 2021",
-                              "Regional share of all philanthropic \nfunding for AQ in 2021", 
-                              "Regional share of all philanthropic \nfunding for AQ in 2021", 
-                              "Percent of countries in region \nthat have fully open AQ data", 
-                              "Percent of countries in region \nthat have fully open AQ data",
-                              "Percent of countries in region \nthat have fully open AQ data",
-                              "Percent of countries in region \nthat have fully open AQ data",
-                              "Percent of countries in region \nthat have fully open AQ data"),
-                  value = c(60.4, 35.2, 2.5, 0.5, 1.4, 67.7, 1.6, 11.9, 2.4, 7.9)) #67.7, 100, 34.9, 4.9, 50
+                  measure = c("Regional share of all philanthropic \nfunding for AQ in 2021^", 
+                              "Regional share of all philanthropic \nfunding for AQ in 2021^", 
+                              "Regional share of all philanthropic \nfunding for AQ in 2021^",
+                              "Regional share of all philanthropic \nfunding for AQ in 2021^", 
+                              "Regional share of all philanthropic \nfunding for AQ in 2021^", 
+                              "Percent of countries in region \nthat have fully open AQ dataº", 
+                              "Percent of countries in region \nthat have fully open AQ dataº",
+                              "Percent of countries in region \nthat have fully open AQ dataº",
+                              "Percent of countries in region \nthat have fully open AQ dataº"),
+                  value = c(60.4, 35.2, 2.5, 0.5, 1.4, 69.2, 1.7, 1.7, 3.4))
 
 df2$region <- factor(df2$region, 
                      levels=c("Europe, US \nand Canada", 
                               "Africa",
                               "Latin America",
+                              "Asia",
                               "Asia (excluding \nIndia and China)",
-                              "Asia (India \nand China)"))
+                              "India and China"))
 
 p1.2 <- df2 %>% ggplot(aes(x = group, y = value , fill = region)) + 
   facet_wrap(~measure, strip.position = "bottom") +
-  geom_bar(stat = 'identity', position = 'stack', width = 0.5) + 
+  geom_bar(stat = 'identity', position = 'stack', width = 0.5, colour = "black") + 
   labs(x = "", y = "", fill = "Region", title = "Resources deployed to address air pollution",
-       caption = "        ") +                         # to even out the x axis
-  scale_fill_manual(values = c("Africa" = "#01497c", 
-                               "Latin America" = "#087e8b", 
+       caption = "Source: º OpenAQ. 2022. “Open Air Quality Data: The global landscape”, ^ Clean Air Fund. 2022. “The State of Global Air Quality Funding”") +                         # to even out the x axis
+  scale_fill_manual(values = c("Africa" = "#01497C", 
+                               "Latin America" = "#6F98A2", 
                                "Europe, US \nand Canada" = "#935c5c", 
-                               "Asia (excluding \nIndia and China)" = "#9bc1bc", 
-                               "Asia (India \nand China)" = "#dde7c7")) + 
+                               "Asia (excluding \nIndia and China)" = "#DDE7C7", 
+                               "India and China" = "#e2e2e2",
+                               "Asia" = "#A6C0B4")) + 
   scale_y_continuous(limits = c(0,100)) +
   ggthemes::theme_tufte() +
   theme(legend.position = "none",
@@ -131,7 +135,7 @@ p1.2 <- df2 %>% ggplot(aes(x = group, y = value , fill = region)) +
         legend.box.margin = margin(b = 1, unit = "cm"),
         plot.subtitle = element_text(hjust = 0.5, size = 7),
         plot.caption = element_text(hjust = 0, size = 9, face = "italic"),
-        legend.key = element_rect(color = "black"),
+        # legend.key = element_rect(color = "black"),
         plot.background = element_rect(color = "white", fill = "white"),
         strip.text.x =  element_text(size = 14),
         strip.placement = "outside",   # format to look like title
@@ -143,25 +147,35 @@ ggsave(glue("{dir}/figures/health_burden_vs_resources.png"), height = 8, width =
 # registry responses map
 interviews <- c( "Argentina", "Bangladesh", "Bolivia", "Cameroon", "Chile", "Colombia",
                  "Cote d’Ivoire", "Gambia", "Ghana", "Guatemala", "Mongolia", "Nigeria",
-                 "Pakistan", "South Africa", "Zimbabwe", "Benin", "Togo")
+                 "Pakistan", "South Africa", "Zimbabwe", "Benin", "Togo", 
+                 "Democratic Republic of the Congo")
 
 registry_countries <- read_excel(glue("{dir}/data/input/unique_registry_responses.xlsx")) %>%
   filter(Country != "United States") %>%
-  mutate(registry = 1,
-         registry_response = ifelse(registry == 1, "Countries with responses to the AQDG registry.\n('*' indicates places where we conducted interviews)", NA),
-         interviewed = ifelse(Country %in% interviews, "*", NA)) %>%
+  mutate(interviewed = ifelse(Country %in% interviews, "*", NA)) %>%
+  left_join(opp_score, by = c("Country" = "country")) %>%
   left_join(gadm0_aqli_2021, by = c("Country" = "name0")) %>%
   st_as_sf()
-  
+
+registry_countries <- registry_countries %>% 
+  mutate(bands = ifelse(is.na(bands) , "Not calculated", bands),
+         bands = ifelse(bands == "Medium" | bands == "Low", "Other", bands))
+
+registry_countries$bands <- factor(registry_countries$bands, 
+                                   levels=c("High", "Medium-high", "Other", "Not calculated"))
+
 registry_map <- ggplot() +
   geom_sf(data = gadm0_aqli_2021, color = "black", fill = "darkgray", lwd = 0.05) +
-  geom_sf(data = registry_countries, mapping = aes(fill = registry_response), 
-          color = "black", lwd = 0.05) +
+  geom_sf(data = registry_countries, mapping = aes(fill = bands), color = "black", lwd = 0.05) +
+  scale_fill_manual(values = c("High" = "#01497C",
+                               "Medium-high" = "#38718F", 
+                               "Other"= "#A6C0B4",
+                               "Not calculated" = "lightgray")) + # check to how to include NA in legend
   geom_sf_text(data = registry_countries, aes(label = interviewed), 
                colour = "white", na.rm = TRUE, size = 6) +
-  scale_fill_manual(values = c("Countries with responses to the AQDG registry.\n('*' indicates places where we conducted interviews)" = "#01497c")) +
   ggthemes::theme_map() +
-  labs(fill = "Response to the AQ \nData Gaps registry") + 
+  labs(fill = "Opportunity Score Bands of the respondents to \nthe AQ Data Gaps registry. ('*' indicates places \nwhere we conducted in-depth interviews)",
+       title = "Countries with responses to the AQ Data Gaps registry") + 
   theme(plot.title = element_text(hjust = 0.5, size = 15), 
         plot.background = element_rect(fill = "#ddeaf5"),
         plot.subtitle = element_text(hjust = 0.5, size = 7), 
@@ -200,12 +214,12 @@ maps_data$non_govt_monitors <- factor(maps_data$non_govt_monitors,
 
 # opportunity score map
 opp_score_map <- ggplot() +
-  geom_sf(data = gadm0_aqli_2021, color = "black", fill = NA,, lwd = 0.05) +
+  geom_sf(data = gadm0_aqli_2021, color = "black", fill = NA, lwd = 0.05) +
   geom_sf(data = maps_data, mapping = aes(fill = bands), color = "black", lwd = 0.05) +
-  scale_fill_manual(values = c("High" = "#01497c",
-                               "Medium-high" = "#087e8b", 
-                               "Medium"= "#9bc1bc", 
-                               "Low"= "#dde7c7", 
+  scale_fill_manual(values = c("High" = "#01497C",
+                               "Medium-high" = "#38718F", 
+                               "Medium"= "#A6C0B4", 
+                               "Low"= "#DDE7C7", 
                                "Not calculated" = "darkgray")) +
   ggthemes::theme_map() +
   labs(fill = "Opportunity to fill air quality data gaps") + 
@@ -228,11 +242,11 @@ ggsave(glue("{dir}/figures/map_opp_score.png"), height = 13, width = 15, opp_sco
 
 # High opportunity score map only
 high_med_bands_map <- ggplot() +
-  geom_sf(data = gadm0_aqli_2021, color = "black", fill = "darkgray",, lwd = 0.05) +
+  geom_sf(data = gadm0_aqli_2021, color = "black", fill = "darkgray", lwd = 0.05) +
   geom_sf(data = maps_data %>% filter(bands == "High" | bands == "Medium-high"), 
           mapping = aes(fill = bands), color = "black", lwd = 0.05) +
-  scale_fill_manual(values = c("High" = "#01497c",
-                               "Medium-high" = "#087e8b")) +
+  scale_fill_manual(values = c("High" = "#01497C",
+                               "Medium-high" = "#38718F")) +
   ggthemes::theme_map() +
   labs(fill = "Opportunity to fill air quality data gaps") + 
   theme(plot.title = element_text(hjust = 0.5, size = 15), 
@@ -257,8 +271,8 @@ funding_map <- ggplot() +
   geom_sf(data = gadm0_aqli_2021, color = "black", fill = NA, lwd = 0.05) +
   geom_sf(data = maps_data, 
           mapping = aes(fill = funding), color = "black", lwd = 0.05) +
-  scale_fill_manual(values = c("Funding > $100,000" = "#dde7c7", 
-                               "Funding =< $100,000" = "#01497c",
+  scale_fill_manual(values = c("Funding > $100,000" = "#DDE7C7", 
+                               "Funding =< $100,000" = "#01497C",
                                "No data or \nhigh income countries" = "darkgray")) +
   ggthemes::theme_map() +
   labs(fill = "International development funding") + 
@@ -283,8 +297,8 @@ ggsave(glue("{dir}/figures/map_funding.png"), height = 13, width = 15, funding_m
 other_mon_map <- ggplot() +
   geom_sf(data = gadm0_aqli_2021, color = "black", fill = NA, lwd = 0.05) +
   geom_sf(data = maps_data, mapping = aes(fill = non_govt_monitors), color = "black", lwd = 0.05) +
-  scale_fill_manual(values = c("At least 1 non-governmental \nair quality monitor" = "#dde7c7",
-                               "No non-governmental \nair quality monitors" = "#01497c",
+  scale_fill_manual(values = c("At least 1 non-governmental \nair quality monitor" = "#DDE7C7",
+                               "No non-governmental \nair quality monitors" = "#01497C",
                                "No data" = "darkgray")) +
   ggthemes::theme_map() +
   labs(fill = "Non-governmental AQ monitoring") + 
